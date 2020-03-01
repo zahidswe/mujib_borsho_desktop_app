27fe2ef68b0daed0767d1ad0ff65d7d45fbfa7a1
zahidswe <zahidswe@gmail.com>
2020-03-01 10:01:46 +0600

    Add project files.

---
+++ MujibBorsoCountDownApp/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace MujibBorsoCountDownApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelDays = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelHours = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.labelMinutes = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.labelSeconds = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Name = "label4";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelTitle
+            // 
+            resources.ApplyResources(this.labelTitle, "labelTitle");
+            this.labelTitle.BackColor = System.Drawing.Color.Transparent;
+            this.labelTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Click += new System.EventHandler(this.labelTitle_Click);
+            // 
+            // labelDays
+            // 
+            resources.ApplyResources(this.labelDays, "labelDays");
+            this.labelDays.ForeColor = System.Drawing.Color.White;
+            this.labelDays.Name = "labelDays";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.labelDays);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // labelHours
+            // 
+            resources.ApplyResources(this.labelHours, "labelHours");
+            this.labelHours.ForeColor = System.Drawing.Color.White;
+            this.labelHours.Name = "labelHours";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panel2.Controls.Add(this.labelHours);
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            // 
+            // labelMinutes
+            // 
+            resources.ApplyResources(this.labelMinutes, "labelMinutes");
+            this.labelMinutes.ForeColor = System.Drawing.Color.White;
+            this.labelMinutes.Name = "labelMinutes";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panel3.Controls.Add(this.labelMinutes);
+            resources.ApplyResources(this.panel3, "panel3");
+            this.panel3.Name = "panel3";
+            // 
+            // labelSeconds
+            // 
+            resources.ApplyResources(this.labelSeconds, "labelSeconds");
+            this.labelSeconds.ForeColor = System.Drawing.Color.White;
+            this.labelSeconds.Name = "labelSeconds";
+            // 
+            // panel4
+            // 
+            resources.ApplyResources(this.panel4, "panel4");
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panel4.Controls.Add(this.labelSeconds);
+            this.panel4.Name = "panel4";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelDays;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelHours;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label labelMinutes;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label labelSeconds;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ MujibBorsoCountDownApp/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MujibBorsoCountDownApp
+{
+    public partial class Form1 : Form
+    {
+        private double timeLeft;
+        private TimeSpan timeLeftMS;
+        private DateTime endDate = DateTime.Parse("17-March-2020");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                var panelRight = (this.Width * 85) / 485;
+                var panelSpace = (this.Width * 60) / 485;
+                this.panel4.Left = this.Width - panelRight;
+                this.panel3.Left = this.panel4.Left - panelSpace;
+                this.panel2.Left = this.panel3.Left - panelSpace;
+                this.panel1.Left = this.panel2.Left - panelSpace;
+
+                var panels = GetAll(this, typeof(Panel));
+
+                foreach (var item in panels)
+                {
+                    item.Height = (this.Height * 78) / 516;
+                    item.Width = item.Height;
+                }
+
+
+                var numberLabelFontSize = (panel4.Width * 24) / 54;
+                var timeLabelFontSize = (numberLabelFontSize * 14) / 24;
+
+                this.labelDays.Font = new Font(labelDays.Font.Name, numberLabelFontSize);
+                this.labelHours.Font = new Font(labelHours.Font.Name, numberLabelFontSize);
+                this.labelMinutes.Font = new Font(labelMinutes.Font.Name, numberLabelFontSize);
+                this.labelSeconds.Font = new Font(labelSeconds.Font.Name, numberLabelFontSize);
+
+                this.label1.Font = new Font(label1.Font.Name, timeLabelFontSize, FontStyle.Bold);
+                this.label2.Font = new Font(label2.Font.Name, timeLabelFontSize, FontStyle.Bold);
+                this.label3.Font = new Font(label3.Font.Name, timeLabelFontSize, FontStyle.Bold);
+                this.label4.Font = new Font(label4.Font.Name, timeLabelFontSize, FontStyle.Bold);
+
+                this.label4.Left = this.panel4.Left + ((panel4.Width - label4.Width) / 2);
+                this.label3.Left = this.panel3.Left + ((panel3.Width - label3.Width) / 2);
+                this.label2.Left = this.panel2.Left + ((panel2.Width - label2.Width) / 2);
+                this.label1.Left = this.panel1.Left + ((panel1.Width - label1.Width) / 2);
+
+                this.label1.Top = this.panel1.Top + panel1.Height;
+                this.label2.Top = this.panel2.Top + panel2.Height;
+                this.label3.Top = this.panel3.Top + panel3.Height;
+                this.label4.Top = this.panel4.Top + panel4.Height;
+
+                this.labelTitle.Font = new Font(labelTitle.Font.Name, (16 * this.Height) / 516);
+                this.labelTitle.Left = this.panel4.Left + panel4.Width - labelTitle.Width;
+                this.labelTitle.Top = label4.Top + label4.Height + ((Height * 25) / 305);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        private const int CP_NOCLOSE_BUTTON = 0x200;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams myCp = base.CreateParams;
+                myCp.ClassStyle = myCp.ClassStyle | CP_NOCLOSE_BUTTON;
+                return myCp;
+            }
+        }
+
+        public IEnumerable<Control> GetAll(Control control, Type type)
+        {
+            var controls = control.Controls.Cast<Control>();
+
+            return controls.SelectMany(ctrl => GetAll(ctrl, type))
+                                      .Concat(controls)
+                                      .Where(c => c.GetType() == type);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            if (ConfigurationManager.AppSettings["EndTime"] != null)
+                endDate = DateTime.Parse(ConfigurationManager.AppSettings["EndTime"]);
+            timeLeft = Math.Ceiling(endDate.Subtract(DateTime.Now).TotalSeconds);
+            if (timeLeft > 0)
+            {
+                updateTimer();
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft--;
+                updateTimer();
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show("Time's up!", "Time has elapsed", MessageBoxButtons.OK);
+            }
+        }
+
+        private void updateTimer()
+        {
+            timeLeftMS = TimeSpan.FromSeconds(timeLeft);
+            labelDays.Text = timeLeftMS.Days.ToString("00");
+            labelHours.Text = timeLeftMS.Hours.ToString("00");
+            labelMinutes.Text = timeLeftMS.Minutes.ToString("00");
+            labelSeconds.Text = timeLeftMS.Seconds.ToString("00");
+        }
+
+        private void labelTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
